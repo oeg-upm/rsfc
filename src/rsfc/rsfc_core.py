43dd36bd42a2090ea498b16c63b81194d61ce2b4
Andres Montero <and.montero.martin@gmail.com>
2025-07-23 16:57:01 +0200

    Fixed minor issues and added functionality

--- src/rsfc/rsfc_core.py
+++ src/rsfc/rsfc_core.py
@@ -6,6 +6,7 @@ import json
 from rsfc.model import assessedSoftware as soft
 from rsfc.rsfc_tests import rsfc_tests as rt
 from importlib.resources import files
+from rsfc.utils import constants, rsfc_helpers
 
 #################################### Auxs Functions ########################################
 
@@ -27,11 +28,11 @@ def somef_assessment(repo_url, threshold):
     return repo_data
 
 
-def render_template(repo_url, checks):
+def render_template(repo_url, repo_type, checks):
     
     data = dict()
     
-    sw = soft.AssessedSoftware(repo_url)
+    sw = soft.AssessedSoftware(repo_url, repo_type)
     
     data['name'] = sw.software_name
     data['url'] = sw.software_url
@@ -56,21 +57,30 @@ def render_template(repo_url, checks):
 
 
 def build_assessment(repo_url):
+    
+    if "github" in repo_url:
+        repo_type = constants.REPO_TYPES[0]
+        repo_branch = None
+    elif "gitlab" in repo_url:
+        repo_type = constants.REPO_TYPES[1]
+        repo_base_api_url = rsfc_helpers.get_repo_api_url(repo_url, repo_type)
+        repo_branch = rsfc_helpers.get_gitlab_default_branch(repo_base_api_url, repo_type)
+    
     somef_data = somef_assessment(repo_url, 0.8)
 
     checks = [
         rt.test_has_license(somef_data),
         rt.test_license_spdx_compliant(somef_data),
-        rt.test_license_info_in_metadata_files(somef_data, repo_url),
+        rt.test_license_info_in_metadata_files(somef_data, repo_url, repo_type, repo_branch),
         rt.test_authors_contribs(somef_data),
         rt.test_authors_orcids(somef_data),
-        rt.test_authors_roles(repo_url),
-        rt.test_metadata_exists(somef_data, repo_url),
-        rt.test_codemeta_exists(repo_url),
+        rt.test_author_roles(repo_url, repo_type, repo_branch),
+        rt.test_metadata_exists(somef_data, repo_url, repo_type, repo_branch),
+        rt.test_codemeta_exists(repo_url, repo_type, repo_branch),
         rt.test_descriptive_metadata(somef_data),
         rt.test_title_description(somef_data),
         rt.test_has_citation(somef_data),
-        rt.test_reference_publication(somef_data, repo_url),
+        rt.test_reference_publication(somef_data, repo_url, repo_type, repo_branch),
         rt.test_software_documentation(somef_data),
         rt.test_readme_exists(somef_data),
         rt.test_contact_support_documentation(somef_data),
@@ -83,22 +93,22 @@ def build_assessment(repo_url):
         rt.test_latest_release_consistency(somef_data),
         rt.test_semantic_versioning_standard(somef_data),
         rt.test_version_scheme(somef_data),
-        rt.test_metadata_record_in_zenodo_or_software_heritage(somef_data, repo_url),
+        rt.test_metadata_record_in_zenodo_or_software_heritage(somef_data, repo_url, repo_type, repo_branch),
         rt.test_id_presence_and_resolves(somef_data),
-        rt.test_id_associated_with_software(somef_data, repo_url),
+        rt.test_id_associated_with_software(somef_data, repo_url, repo_type, repo_branch),
         rt.test_id_proper_schema(somef_data),
         rt.test_identifier_in_readme_citation(somef_data),
         rt.test_identifier_resolves_to_software(somef_data),
-        rt.test_presence_of_tests(repo_url),
+        rt.test_presence_of_tests(repo_url, repo_type, repo_branch),
         rt.test_github_action_tests(somef_data),
         rt.test_repository_workflows(somef_data),
         rt.test_is_github_repository(repo_url),
-        rt.test_repo_enabled_and_commits(somef_data, repo_url),
+        rt.test_repo_enabled_and_commits(somef_data, repo_url, repo_type, repo_branch),
         rt.test_has_tickets(somef_data),
         rt.test_repo_status(somef_data)
     ]
     
-    results = render_template(repo_url, checks)
+    results = render_template(repo_url, repo_type, checks)
     
     output_path = './outputs/rsfc_assessment.json'
 
