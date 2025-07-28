6dd5c550d88551140140c4602c4c13eb7fba0c3f
Andres Montero <and.montero.martin@gmail.com>
2025-07-28 10:46:25 +0200

    Updated AssessedSoftware class

--- src/rsfc/model/assessedSoftware.py
+++ src/rsfc/model/assessedSoftware.py
@@ -2,17 +2,20 @@ import urllib
 from urllib.parse import unquote
 import requests
 from datetime import datetime
+from rsfc.utils import constants, rsfc_helpers
 
 class AssessedSoftware:
-    def __init__(self, repo_url, repo_type):
+    def __init__(self, repo_url):
         self.software_url = repo_url
-        base_url = self.get_repo_base_url(repo_url, repo_type)
+        self.repo_type = self.get_repo_type(repo_url)
+        base_url = self.get_repo_base_url(repo_url)
         self.software_name = self.get_soft_name(unquote(base_url))
-        self.software_version = self.get_soft_version(base_url, repo_type)
+        self.software_version = self.get_soft_version(base_url)
         self.software_id = None
+        self.repo_branch = rsfc_helpers.get_gitlab_default_branch(base_url, self.repo_type)
         
         
-    def get_repo_base_url(self, repo_url, repo_type):
+    def get_repo_base_url(self, repo_url):
         parsed_url = urllib.parse.urlparse(repo_url)
         path_parts = parsed_url.path.strip("/").split("/")
         if len(path_parts) < 2:
@@ -20,26 +23,15 @@ class AssessedSoftware:
 
         owner, repo = path_parts[-2], path_parts[-1]
 
-        if repo_type == 'GITHUB':
+        if self.repo_type == constants.REPO_TYPES[0]:
             url = f"https://api.github.com/repos/{owner}/{repo}"
-        elif repo_type == "GITLAB":
+        elif self.repo_type == constants.REPO_TYPES[1]:
             project_path = urllib.parse.quote(f"{owner}/{repo}", safe="")
             url = f"https://gitlab.com/api/v4/projects/{project_path}"
         else:
             raise ValueError("URL not within supported types (Github and Gitlab)")
 
         return url
-        
-    '''def get_base_url(self, repo_url, repo_type):
-        parsed_url = urllib.parse.urlparse(repo_url)
-        path_parts = parsed_url.path.strip("/").split("/")
-        if len(path_parts) < 2:
-            raise ValueError("Error when getting Github API URL")
-        owner, repo = path_parts[-2], path_parts[-1]
-        
-        url = f"https://api.github.com/repos/{owner}/{repo}"
-        
-        return url'''
         
         
     def get_soft_name(self, base_url):
@@ -85,33 +77,10 @@ class AssessedSoftware:
             return None
 
 
-
-    '''def get_soft_version(self, url):
-        try:
-            releases_url = f"{url}/releases"
-            response = requests.get(releases_url)
-            response.raise_for_status()
-            releases = response.json()
-
-            latest_release = None
-            latest_date = None
-            for release in releases:
-                date_str = release.get("published_at")
-                tag = release.get("tag_name")
-                if date_str and tag:
-                    try:
-                        dt = datetime.fromisoformat(date_str.rstrip("Z"))
-                    except ValueError:
-                        continue
-
-                    if latest_release is None or dt > latest_date:
-                        latest_release = tag
-                        latest_date = dt
-
-            return latest_release
-
-        except Exception as e:
-            print(f"Error fetching releases from GitHub: {e}")
-            return None'''
-
-
+    def get_repo_type(self, repo_url):
+        if "github" in repo_url:
+            repo_type = constants.REPO_TYPES[0]
+        elif "gitlab" in repo_url:
+            repo_type = constants.REPO_TYPES[1]
+            
+        return repo_type
